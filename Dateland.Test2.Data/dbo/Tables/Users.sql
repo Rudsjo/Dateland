20174afb315bf12cb08551bf71d3442c8377c0bc
Rudsjo <56698997+Rudsjo@users.noreply.github.com>
2020-09-18 16:33:52 +0200

    Cleanup for new functions

--- Dateland.Test2.Data/dbo/Tables/Users.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Users]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-	[FirstName] nvarchar(50) NOT NULL,
-	[LastName] nvarchar(50) NOT NULL,
-	[EmailAdress] nvarchar(50) NOT NULL,
-	[ProfilePictureUrl] nvarchar(MAX) NULL
-)
